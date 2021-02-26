66e93998a0acb267fa3c88e11f83293330a14bed
Jeremy Leipzig <leipzig@gmail.com>
2021-02-26 14:34:33 -0500

    plot

--- biopick.R
+++ biopick.R
@@ -3,10 +3,10 @@ library(dplyr)
 library(lubridate)
 library(ggthemes)
 
-biopick<-read.table("Biopick2021-wk1.csv",header=TRUE)
-pickFreq<-as.data.frame(sort(table(biopick),decreasing = TRUE))
-names(pickFreq)<-c("ticker","freq")
-batchres<-BatchGetSymbols(
+biopick <- read.table("Biopick2021-wk1.csv", header = TRUE)
+pickFreq <- as.data.frame(sort(table(biopick), decreasing = TRUE))
+names(pickFreq) <- c("ticker", "freq")
+batchres <- BatchGetSymbols(
   pickFreq$ticker,
   first.date = "2021-01-04",
   last.date = Sys.Date(),
@@ -27,14 +27,31 @@ prices <- batchres$df.tickers %>%
   merge(pickFreq) %>%
   mutate(datething = lubridate::as_datetime(ref.date)) %>%
   group_by(ticker) %>%
-  dplyr::mutate(nrml_price = price.close*100/first(price.close)-100)
+  dplyr::mutate(nrml_price = price.close * 100 / first(price.close))
 
-ggplot(prices %>% filter(freq>10),aes(ref.date,nrml_price))+geom_line(aes(color=ticker,size=freq))
+ggplot(prices %>% filter(freq > 10), aes(ref.date, nrml_price)) + geom_line(aes(color =
+                                                                                  ticker, size = freq))
 
-lastdate<-max(prices$datething)
-results<-prices %>% dplyr::filter(datething==lastdate) %>% dplyr::select(nrml_price,freq)
+lastdate <- max(prices$datething)
+results <-
+  prices %>% dplyr::filter(datething == lastdate) %>% dplyr::select(nrml_price, freq)
 
-ggplot(results,aes(freq,nrml_price)) + 
+ggplot(results, aes(freq, nrml_price)) +
   geom_point() +
-  geom_text(data=subset(results, freq>15 | nrml_price>200), aes(freq,nrml_price,label=paste0(ticker)),check_overlap = FALSE,hjust = 1.2, nudge_y = 0.5) +
-  scale_y_continuous(breaks=seq(-100,1000,100))
+  geom_text(
+    data = subset(results, freq > 15 |
+                    nrml_price > 300),
+    aes(freq, nrml_price, label = paste0(ticker)),
+    check_overlap = FALSE,
+    position = position_jitter(width = 2, height = 2),
+    hjust = 1.2
+  ) +
+  geom_text(
+    data = subset(results, nrml_price < 50),
+    aes(freq, nrml_price, label = paste0(ticker)),
+    check_overlap = FALSE,
+    position = position_jitter(width = 2, height = 2),
+    vjust = 1.2
+  ) +
+  scale_y_continuous(breaks = seq(0, 1000, 100)) + 
+  xlim(-5, 70)+xlab("Number of Picks")+ylab("Percent of Price on 01/04/2021")
